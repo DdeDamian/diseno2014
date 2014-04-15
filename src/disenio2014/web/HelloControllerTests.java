939ce4986310de1e770a2fa5d99f276b9ce2b854
Damian Velazquez Cafaro <enviaaca@gmail.com>
2014-04-15 03:45:11 -0300

    First full spring example working

---
+++ src/disenio2014/web/HelloControllerTests.java
@@ -0,0 +1,16 @@
+package disenio2014.web;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import disenio2014.web.HelloController;
+
+import junit.framework.TestCase;
+
+public class HelloControllerTests extends TestCase {
+
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+}
